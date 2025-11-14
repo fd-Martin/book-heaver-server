f70e114899571840ce89e42047e762598a824272
fd-Martin <fdmosharof@gmail.com>
2025-11-14 22:51:05 +0600

    done

--- index.js
+++ index.js
@@ -27,26 +27,44 @@ async function run() {
 
     const db = client.db("book_db");
     const booksCollection = db.collection("books");
+    const commentsCollection = db.collection("comments"); //
 
-    app.get("/books", async (req, res) => {
-      const cursor = booksCollection.find();
+    app.get("/all-books", async (req, res) => {
+      const cursor = booksCollection.find().sort({ rating: -1 });
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/books/:id", async (req, res) => {
+    app.get("/my-books", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+      const cursor = booksCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/book-details/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await booksCollection.findOne(query);
       res.send(result);
     });
 
-        app.post("/books", async (req, res) => {
+    app.post("/add-book", async (req, res) => {
       const newBook = req.body;
       const result = await booksCollection.insertOne(newBook);
       res.send(result);
     });
 
-        app.patch("/books/:id", async (req, res) => {
+    app.get("/latest-books", async (req, res) => {
+      const cursor = booksCollection.find().sort({ rating: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.patch("/update-book/:id", async (req, res) => {
       const id = req.params.id;
       const updateBook = req.body;
       const query = { _id: new ObjectId(id) };
@@ -57,16 +75,34 @@ async function run() {
       res.send(result);
     });
 
-    
-    app.delete("/books/:id", async (req, res) => {
+    app.delete("/delete-book/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await booksCollection.deleteOne(query);
       res.send(result);
     });
-    
+
+    //com
+    app.get("/comments/:bookId", async (req, res) => {
+      const bookId = req.params.bookId;
+      const query = { bookId };
+      const comments = await commentsCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
+      res.send(comments);
+    });
+
+    app.post("/add-comment", async (req, res) => {
+      const newComment = req.body;
+      newComment.createdAt = new Date();
+
+      const result = await commentsCollection.insertOne(newComment);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
